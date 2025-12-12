7f9b08e9c7c510407dee9cb83a70227717526e55
Leonie Chen <leochen@f0r3s14.clusters.42paris.fr>
2025-12-12 20:13:15 +0100

    first

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+// express() creates an instance of an express application 
+const app = express();  
+
+app.get("/api/notes", (req,res) => {
+    res.send("you got 5 notes");
+});
+
+//listen() method is used to bind and listen the connections on the specified host and port, console.log() is used to print the message on the console once the server starts
+app.listen(5001, ()=> {
+    console.log("Server started on port 5001")  
+});
+
+
+
+
+
+
+
+
+//notes 
+// 1, npm run xxx 里的 xxx 必须和 pacakage.json里面scripts 里的 key 一模一样。
+//2.github 配置传，依赖不传；规则传，秘密不传。
